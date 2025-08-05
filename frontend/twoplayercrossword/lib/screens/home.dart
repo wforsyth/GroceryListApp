a02b400c978ff9f6f7db4a88585a34629249f263
William Forsyth <williamforsyth@MacBookAir.lan>
2025-08-05 15:23:29 +0100

    new home button

--- frontend/twoplayercrossword/lib/screens/home.dart
+++ frontend/twoplayercrossword/lib/screens/home.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import '../constants.dart';
+import '../routes.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: kSurfaceColor,
       body: Center(
-        child: Text(
-          'Home',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'You have no lists',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              "Let's create your first",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, AppRoutes.home);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kPrimaryColor,
+              ),
+              child: const Text('Create a List'),
+            ),
+          ],
         ),
       ),
     );
